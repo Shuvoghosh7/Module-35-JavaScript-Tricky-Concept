d85dccd8b5f09f5f5bafbb9ca4d2f376ca4062f2
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-25 20:42:22 +0600

    add some concepts

---
+++ data-type.js
@@ -0,0 +1,28 @@
+let p='Javascript';
+ let q=p; 
+ p='React'; 
+// console.log(q)
+// Q-3
+const isTrue='false'; 
+if(!isTrue){
+console.log('hello');
+} else {
+// console.log('world');
+ }
+//Q-6
+function sum(p, q) {
+    p + q;
+   }
+   const result = sum(2, 3);
+//    console.log(result);
+   //Q-8
+   if ("2" === 2) {
+    // console.log("Inside if");
+   } else {
+    // console.log("Inside else");
+   }
+   //9
+   function work(x, y = 4) {
+    return x + y;
+   }
+   console.log(work(32);
